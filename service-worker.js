ee030bd921f85443313b7e454fe3ff01f2c51f34
Sara Waclawiec <sara.ems.94@gmail.com>
2019-02-18 08:54:20 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dddf48e8f8427e93a5cb089c12d57d0.js"
+  "/Mountain_Kingdom/precache-manifest.bbf62b62cb81214686a55f4c1fc44805.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Mountain_Kingdom/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
