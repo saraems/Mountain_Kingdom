e5fae1637d0bee74fdbb30d89e6ac5817c48c307
Sara Waclawiec <sara.ems.94@gmail.com>
2019-02-18 16:37:54 +0100

    RWD

--- src/components/LogIn/Login.js
+++ src/components/LogIn/Login.js
@@ -6,7 +6,7 @@ class Login extends Component {
     return (
         <div className="login_subpage">
           <div className="login_window">
-                <div>Are you a Mountain Knight?</div>
+                <div className='login_window__header'>Are you a Mountain Knight?</div>
                 <input onChange={ this.props.login } type="email" placeholder="Email"/>
                 <input onChange={ this.props.password } type="password" placeholder="Password"/>
                 <button onClick={ this.props.submit } className="login_btn btn_post">Log in</button>
